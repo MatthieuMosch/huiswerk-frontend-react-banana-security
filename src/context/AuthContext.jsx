6e98417170490a13e016edbd2e782e0654c07581
MatthieuMosch <mmosch@gmail.com>
2025-03-29 18:22:29 +0100

    met state

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,20 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 
 export const AuthContext = createContext(null);
 
 function AuthContextProvider({children}) {
+    const [auth, setAuth] = useState({username: "", email: "", isAuth: false});
+
+    function login() {
+        setAuth({...auth, isAuth: true});
+    }
+
+    function logout() {
+        setAuth({...auth, isAuth: false});
+    }
 
     return (
-        <AuthContext.Provider value={{username: "naam", email: "e-mail", isAuthenticated: false,}}>
+        <AuthContext.Provider value={auth}>
             {children}
         </AuthContext.Provider>
     );
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,26 @@
 import {useContext} from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {AuthContext} from "../context/AuthContext";
 
 function Profile() {
     const auth = useContext(AuthContext);
-  return (
-    <>
-      <h1>Profielpagina</h1>
-      <section>
-        <h2>Gegevens</h2>
-        <p><strong>Gebruikersnaam:</strong> {auth.username}</p>
-        <p><strong>Email:</strong> {auth.email}</p>
-      </section>
-      <section>
-        <h2>Strikt geheime profiel-content</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
-      </section>
-      <p>Terug naar de <Link to="/">Homepagina</Link></p>
-    </>
-  );
+    return (
+        <>
+            <h1>Profielpagina</h1>
+            <section>
+                <h2>Gegevens</h2>
+                <p><strong>Gebruikersnaam:</strong> {auth.username}</p>
+                <p><strong>Email:</strong> {auth.email}</p>
+                <p><strong>ingelogd:</strong> {auth.isAuth.toString()}</p>
+            </section>
+            <section>
+                <h2>Strikt geheime profiel-content</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id
+                    molestias qui quo unde?</p>
+            </section>
+            <p>Terug naar de <Link to="/">Homepagina</Link></p>
+        </>
+    );
 }
 
 export default Profile;
